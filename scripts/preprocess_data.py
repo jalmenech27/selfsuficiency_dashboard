45839dbe255e1ff3ae6f8aa9ed9e7bd9a6271bbf
Jordi AD <56399137+jalmenech27@users.noreply.github.com>
2025-06-13 12:37:00 +0200

    Update preprocess_data.py

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Script per preprocessar les dades de FAOSTAT descarregades per al projecte d'Autosuficiència Alimentària
 Author: Jordi Almiñana Domènech (UOC Visualització de Dades)
